b8e39e4eba78227cf082ac1b41e01cf8e9f6e087
tmkcnameserchkr <fazolumar4@gmail.com>
2026-01-29 13:14:40 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -357,7 +357,6 @@ if __name__ == '__main__':
     ping_thread.start()
 
 
-    app.run(host='0.0.0.0', port=10000)
-
-
-
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
